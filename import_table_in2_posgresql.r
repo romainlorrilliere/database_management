df00817e94fe9926613cc5e9832cca5e8fea05a0
Romain Lorrilliere <romainlorrilliere@gmail.com>
2018-12-10 12:41:33 +0100

    work is still in progress

--- import_table_in2_posgresql.r
+++ import_table_in2_posgresql.r
@@ -35,7 +35,12 @@ myshell <- function(mycmd,myinvisible=TRUE) {
 
 
 
-import_psql <- function(file="testAmphi2.csv",vecSep=c(",",";","\t"),pathData="rawData",pathSQL= "sql",changeColNames=c("collaborative_australian_protected_areas_database__capad__marine_2010"="capad__marine_2010","collaborative_australian_protected_areas_database__capad__marine_2010_1"="capad__marine_2010_1"), excludedColumn=NULL,#"taxon_identification_issue",
+psql_import_table <- function(file="testAmphi2.csv",
+                              vecSep=c("\t",";",","),
+                              pathData="rawData",pathSQL= "sql",
+                              changeColNames=c("collaborative_australian_protected_areas_database__capad__marine_2010"="capad__marine_2010",
+                                               "collaborative_australian_protected_areas_database__capad__marine_2010_1"="capad__marine_2010_1"),
+                              excludedColumn=NULL,#"taxon_identification_issue",
                         fileSQL= "_postgres_import_data.sql",
                         db.name="harmony",db.user="postgres",
                         tableName="amphibians",pkField="record_id",
@@ -43,7 +48,7 @@ import_psql <- function(file="testAmphi2.csv",vecSep=c(",",";","\t"),pathData="r
                         toDoLiteTable=FALSE,litetableName="alalite",
                         keepedCol=NULL,litetableColType=c("f"="varchar(20)","e"="real"),
                         vecIndex=NULL,
-                        createGeom=FALSE,geomName="geom",epsgRaw="",epsgGeom=NULL,geom.lonlat=c("lon","lat")) {
+                        createGeom=TRUE,geomName=NULL,epsgRaw="4326",epsgGeom=NULL,geom.lonlat=c("decimallongitude","decimallatitude")) {
 
 
     ## ---- initializing parameters for debugging ----
@@ -56,11 +61,19 @@ import_psql <- function(file="testAmphi2.csv",vecSep=c(",",";","\t"),pathData="r
     ## -------------------------------------------------------
 
 
+    cat("\n Table importation in psql database\n======================================\n\n")
+
+    cat("Summary:\n-------------\n\n")
+
+
+
     fileSQL.path <- paste(pathSQL,"/",fileSQL,sep="")
 
     if(is.null(pathData)) pathDataFull <- paste(getwd()) else pathDataFull <- paste(getwd(),"/",pathData,"/",sep="")
 
     pathFile <- paste(pathDataFull,file,sep="")
+
+    cat(" psql   [",tableName,"] <- ",pathFile,"\n",sep="")
 
     flag <- TRUE
     i <- 0
@@ -72,25 +85,71 @@ import_psql <- function(file="testAmphi2.csv",vecSep=c(",",";","\t"),pathData="r
     }
     h <- colnames(d)
     h <- tolower(h)
-    #browser()
-    h <- gsub(".","_",h,fixed =TRUE)
-    h[h=="class"] <- "classe"
-    h[h=="order"] <- "ordre"
 
-    if(!is.null(excludedColumn))
+
+
+
+    cat("\n",length(h),"columns detected with the separator:",theSeparator,"\n\n")
+
+
+    cat("\nFixing some pb in data header:\n----------------------\n\n")
+
+    whereRsep <- grep(theSeparator,h,fixed=TRUE)
+    if(length(whereRsep)>0) {
+        cat("",length(whereRsep)," separator character detected in colnames have to be changed \n ",whereRsep," -> _ \n\n",sep="")
+        h <- gsub(theSeparator,"_",h,fixed =TRUE)
+        }
+
+    whereRdot <- grep(".",h,fixed=TRUE)
+    if(length(whereRdot)>0) {
+        cat("",length(whereRdot)," dot(s) detected have to be changed \n . -> _ \n\n",sep="")
+        h <- gsub(".","_",h,fixed =TRUE)
+        }
+
+
+    whereRcomma <- grep(",",h,fixed=TRUE)
+    if(length(whereRcomma)>0) {
+        cat("",length(whereRcomma)," comma(s) detected have to be changed \n , -> _ \n\n",sep="")
+        h <- gsub(",","_",h,fixed =TRUE)
+        }
+
+
+    whereRsemicolon <- grep(";",h,fixed=TRUE)
+    if(length(whereRsemicolon)>0) {
+        cat("",length(whereRsemicolon)," semicolon(s) detected have to be changed \n ; -> _ \n\n",sep="")
+        h <- gsub(";","_",h,fixed =TRUE)
+        }
+
+
+    if(!is.null(excludedColumn)) {
+        cat("Excluding",length(excludedColumn),"column(s)\n")
         h <- h[!(h%in%excludedColumn)]
+        }
+    if(!is.null(changeColNames)) {
+        cat("Changing",length(changeColNames),"column names\n")
+        for(i in 1:length(changeColNames))
+            h[h==names(changeColNames)[1]] <- changeColNames[1]
 
-    for(i in 1:length(changeColNames))
-                            h[h==names(changeColNames)[1]] <- changeColNames[1]
+    }
 
 
-    cat("\n",length(h)," columns founded with the separator:",theSeparator,"\n")
+
+    if("class" %in% h) {
+        cat("changing colname class to classe\n")
+        h[h=="class"] <- "classe"
+    }
+    if("order" %in% h) {
+        cat("changing colname order to ordre\n")
+        h[h=="order"] <- "ordre"
+    }
+
+
+
 
     head <- paste("DROP table if exists ",tableName,";\nCREATE TABLE ",tableName,"\n(",sep="")
 
     end <- ");\n"
 
-#browser()
 
 
     field <- paste(paste(h,ifelse(h %in% realField," real",
@@ -103,17 +162,27 @@ import_psql <- function(file="testAmphi2.csv",vecSep=c(",",";","\t"),pathData="r
     create <- paste(head,field,end,collapse="")
 
 
+
     if(fileSQL%in% dir(pathSQL)) file.remove(fileSQL.path)
 
+
+    cat("\nThe queries:\n-------------\n\n")
+    cat("   SQL file:",fileSQL.path,"\n\n")
+
+
+#    if(nchar(create)>1000) cat(substr(create,1,500),"\n[...CREATE query to long to be enterely showed...]\n",substr(create,nchar(create)-50,nchar(create)),"\n") else cat(create,"\n")
     cat(create,file=fileSQL.path,append=TRUE)
 
-    cat("\n\n\ \\copy ",tableName,ifelse(!is.null(excludedColumn),paste("(",paste(h,collapse=","),")",sep=""),"")," FROM '",pathFile,"' with (format csv, header, delimiter '",theSeparator,"',ESCAPE '\"', null '',ENCODING 'UTF-8')\n",sep="",file=fileSQL.path,append=TRUE)
+    copyQuery <- paste("\n\n\ \\copy ",tableName,ifelse(!is.null(excludedColumn),paste("(",paste(h,collapse=","),")",sep=""),"")," FROM '",pathFile,"' with (format csv, header, delimiter '",theSeparator,"', ESCAPE ',', null '',ENCODING 'UTF-8')\n",sep="")#,ESCAPE '\"'
+    cat(copyQuery)
+    cat(copyQuery,file=fileSQL.path,append=TRUE)
 
     if(toDoLiteTable) {
 
         query <- paste("\n\nDROP TABLE IF EXISTS ",litetableName,
                        ";\nCREATE TABLE ",litetableName," as (\nSELECT ",
                        paste(keepedCol,collapse=",")," from ",tableName,");\n")
+        cat(query)
         cat(query,file=fileSQL.path,append=TRUE)
         if(!is.null(litetableColType)) {
             query <- paste("\n",paste("ALTER TABLE ",litetableName," ALTER COLUMN ",names(litetableColType)," TYPE ",litetableColType,";\n"),collapse="")
@@ -122,19 +191,23 @@ import_psql <- function(file="testAmphi2.csv",vecSep=c(",",";","\t"),pathData="r
     }
 
     if(createGeom) {
-        if(is.null(epsgRaw)) epsgRaw <- "4326"
-        if(is.null(epsgRaw)) epsgGeom <- epsgRaw
+        if(is.null(epsgGeom)) epsgGeom <- epsgRaw
+        if(is.null(geomName)) geomName <- paste("geom",epsgGeom,sep="")
 
-        query <- paste("ALTER TABLE ",ifelse(toDoLiteTable,litetableName,tableName)," ADD COLUMN ",geomName," geometry(Point,",epsgGeom,");\nUPDATE ",ifelse(toDoLiteTable,litetableName,tableName),"\nSET ",geomName," = ",ifelse(epsgRaw!=epsgGeom,"ST_Transform(",""),"ST_SetSRID(ST_MakePoint(", geom.lonlat[1],",", geom.lonlat[2],"),",epsgRaw,")",ifelse(epsgRaw!=epsgGeom,",2154)",""),";\n",sep="")
+        query <- paste("\nALTER TABLE ",ifelse(toDoLiteTable,litetableName,tableName)," ADD COLUMN ",geomName," geometry(Point,",epsgGeom,");\nUPDATE ",ifelse(toDoLiteTable,litetableName,tableName),"\nSET ",geomName," = ",ifelse(epsgRaw!=epsgGeom,"ST_Transform(",""),"ST_SetSRID(ST_MakePoint(", geom.lonlat[1],",", geom.lonlat[2],"),",epsgRaw,")",ifelse(epsgRaw!=epsgGeom,",2154)",""),";\n",sep="")
+        cat(query)
         cat(query,file=fileSQL.path,append=TRUE)
     }
 
 
     if(!is.null(vecIndex)){
-        query <- paste(paste("CREATE INDEX index_",vecIndex," ON ",ifelse(toDoLiteTable,litetableName,tableName),"(vecIndex);\n",sep=""),collapse="")
+        query <- paste("\n",paste("CREATE INDEX index_",vecIndex," ON ",ifelse(toDoLiteTable,litetableName,tableName),"(vecIndex);\n",sep=""),collapse="")
+        cat(query)
         cat("\n",query,file=fileSQL.path,append=TRUE)
     }
 
+
+    cat("\n==============================\n\n")
     pathSQLfull <- paste(getwd(),"/",pathSQL,"/",sep="")
     cmd_import <- paste("psql -U ",db.user," ",db.name," < ",pathSQLfull,"_postgres_import_data.sql",sep="")
     myshell(cmd_import)
